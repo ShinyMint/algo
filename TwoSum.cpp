c04da99ee2703faa15fe8d6ba4a62a884f335069
ShinyMint <59549930+ShinyMint@users.noreply.github.com>
2021-01-13 20:18:05 +0900

    Create TwoSum.cpp

---
+++ TwoSum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> answer; //store the answer
+        int numCheck; //stores the difference(gap)
+        
+        for(int i = 0; i < nums.size(); i++) { //check from nums[0]~nums[nums.size()-1]
+            numCheck = target - nums[i]; //check the difference between target & nums[i]
+            
+            for(int j = i + 1; j < nums.size(); j++) {
+                if (nums[j] == numCheck) { //if the difference can be found in 'nums', 
+                    answer.push_back(i); //add position into 'answer' vector
+                    answer.push_back(j); 
+                    break; //this requires exactly one solution. So if you find answer, break
+                }
+            }
+        }
+        return answer;
+    }
+};
